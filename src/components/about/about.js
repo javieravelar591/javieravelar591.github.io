88f7b4498ea45a0823c4f4d3040e530a87d3bfb3
Javier Avelar <avela020@umn.edu>
2024-01-03 17:34:29 -0600

    more styling changes

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -30,15 +30,15 @@ export const About = () => {
               <div className={`about-content ${isAnimated ? 'slide-up' : ''}`}>
                     <h1 className='about-header'>¡Hola! I'm Javi,</h1>
                     <span className='about-span'>
-                    I'm a motivated, collaborative, and test developer driven by my mission to make making technology accessible for the betterment of everyone.
-                    I believe my experience and education in software development have enabled me to collaborate and communicate effectively with cross-disciplinary team members to 
-                    create effecient, effective, and empathetic solutions. <br />
+                    I'm a passionate and collaborative developer on a mission to make technology accessible for everyone's benefit. My experience in software development, coupled with a degree in Computer Science, equips me to create efficient, effective, and empathetic solutions. <br />
                     <h2>A little more about me:</h2>
-                    I'm a first-generation Latinx student who has recently completed a bachelor's degree in Computer Science. I grew up in an immigrant household with limited-English speaking parents. As a result,
-                    I've witnessed firsthand the repercussions of non-accessible technology and unempathetic solutions. This fuels my commitment to creating solutions that break down barriers and serve diverse communities.
+                    {/* I'm a first-generation Latinx student who has recently completed a bachelor's degree in Computer Science. I grew up in an immigrant household with limited-English speaking parents. As a result,
+                    I've witnessed firsthand the repercussions of non-accessible technology and unempathetic solutions. This fuels my commitment to creating solutions that break down barriers and serve diverse communities. */}
+                    As a first-generation Latinx student, I bring a unique perspective shaped by my upbringing in an immigrant household. Witnessing the impact of non-accessible technology drives my commitment to breaking down barriers and serving diverse communities.
                     <br />
-                    As a tech enthusiast, I make it a point to stay on the pulse of the industry, constantly absorbing the latest news and trends. It's my way of staying connected to the dynamic landscape of innovation.
-                    I am extremely interested in the field of Artificial Intelligence and its applications to increase the standard of living.
+                    {/* As a tech enthusiast, I make it a point to stay on the pulse of the industry, constantly absorbing the latest news and trends. It's my way of staying connected to the dynamic landscape of innovation.
+                    I am extremely interested in the field of Artificial Intelligence and its applications to increase the standard of living. */}
+                    Beyond coding, I stay abreast of industry trends and emerging technologies, particularly in Artificial Intelligence. I'm fascinated by its potential to elevate our quality of life.
                     <br />
                     When I'm not immersed in the tech world, you'll find me engaged in the art of combat sports, skateboarding, or simply enjoying of a well-brewed cup of coffee. &#9749;
                     </span>
